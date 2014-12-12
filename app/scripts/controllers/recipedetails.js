eed44be65b102257d0cb2247aad5976d538041be
tkarling <tkarling@live.com>
2014-12-11 17:45:24 -0800

    some cleanup

--- app/scripts/controllers/recipedetails.js
+++ app/scripts/controllers/recipedetails.js
@@ -8,30 +8,11 @@
  * Controller of the recipeshopperApp
  */
 
-var UploadController = function ($scope, fileReader) {
-    console.log('UploadController' + fileReader);
-    $scope.getFile = function () {
-        $scope.progress = 0;
-        console.log('getFile');
-        fileReader.readAsDataUrl($scope.file, $scope)
-          .then(function(result) {
-              $scope.imageSrc = result;
-          });
-    };
- 
-    $scope.$on("fileProgress", function(e, progress) {
-        $scope.progress = progress.loaded / progress.total;
-    });
-
-    $scope.imageSrc = 'images/yeoman.png';
- 
-};
-
 angular.module('recipeshopperApp')
   .controller('RecipeDetailsController', function ($scope, fileReader) {
     currentTab=2;
 
-    console.log('RecipeDetailsController' + fileReader)
+    console.log('RecipeDetailsController' + fileReader);
     $scope.getFile = function () {
         $scope.progress = 0;
         console.log('getFile');
@@ -41,30 +22,17 @@ angular.module('recipeshopperApp')
           });
     };
 
-
-
-    // $scope.getFile = function () {
-    //     // $scope.progress = 0;
-    //     fileReader.readAsDataUrl($scope.file, $scope)
-    //       .then(function(result) {
-    //           $scope.imageSrc = result;
-    //       });
-    // };
-    // // $scope.imageSrc = 'images/yeoman.png';
-
   })
-  .directive("ngFileSelect",function(){
-
+  .directive('ngFileSelect', function(){
 	  return {
-	    link: function($scope,el){
+	    link: function($scope, el) {
 	      console.log('ngFileSelect');	
 	      
-	      el.bind("change", function(e){
-	      
+	      el.bind('change', function(e) {
 	        $scope.file = (e.srcElement || e.target).files[0];
 	        console.log('file: '+ $scope.file);	
 	        $scope.getFile();
-	      })
+	      });
 	    }
-	  }
+	  };
   });
