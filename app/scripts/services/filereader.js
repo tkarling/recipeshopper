6d4b33f1ef0c837064e52e021d92036cf6d38ec9
tkarling <tkarling@live.com>
2014-12-12 12:11:50 -0800

    moved file select directive in the same file as the service it uses

--- app/scripts/services/filereader.js
+++ app/scripts/services/filereader.js
@@ -62,4 +62,38 @@
         }
 
       };
-   });
+   })
+
+  .directive('rsFileSelect', function(fileReader){
+      return {
+        link: function(scope, element, attrs) {
+          // console.log('ngFileSelect'); 
+          var imageSrcs = scope[attrs['imageSrcs']];
+
+        var getFile = function (filesRead) {
+            // console.log('getFile');
+            // fileReader.moi();
+
+            imageSrcs = [];  
+            for(var i=0; i < filesRead.length; i++) {
+              fileReader.readAsDataUrl(filesRead[i], scope)
+                .then(function(result) {
+                    var index = imageSrcs.length;
+                    imageSrcs[index] = result;
+                    // console.log('result: ' + result);
+                    // console.log('imageSrcs['+ index + ']: ' + imageSrcs[index]);
+                });
+            }
+
+        scope[attrs['imageSrcs']] = imageSrcs;
+          };
+          
+          element.bind('change', function(e) {
+            var filesRead = (e.srcElement || e.target).files;
+            getFile(filesRead);
+          });
+
+        }
+      };
+    });
+
