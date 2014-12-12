2a7c9da9cbd365dba2749056b7b2d8636b2de6f0
tkarling <tkarling@live.com>
2014-12-12 12:01:06 -0800

    removed fileSelect directives dependency of scope's properties. & some Clean up

--- app/scripts/controllers/recipedetails.js
+++ app/scripts/controllers/recipedetails.js
@@ -9,62 +9,38 @@
  */
 
 angular.module('recipeshopperApp')
-  .controller('RecipeDetailsController', ['$scope', function ($scope) { // fileReader
+  .controller('RecipeDetailsController', ['$scope', function ($scope) { 
     currentTab=2;
-    // fileReader.moi();
-
-    // console.log('RecipeDetailsController' + fileReader);
-    // $scope.getFile = function () {
-    //     $scope.progress = 0;
-    //     console.log('getFile');
-    //     fileReader.moi();
-    //     fileReader.readAsDataUrl($scope.file, $scope)
-    //       .then(function(result) {
-    //           $scope.imageSrc = result;
-    //       });
-    // };
-
   }])
+
   .directive('rsFileSelect', function(fileReader){
 	  return {
-	    link: function(scope, element) {
-	      console.log('ngFileSelect');	
-	      // var fileRead = {};
+	    link: function(scope, element, attrs) {
+	      // console.log('ngFileSelect');	
+	      var imageSrcs = scope[attrs['imageSrcs']];
 
-		  scope.getFile = function (fileRead, filesRead) {
-	        // scope.progress = 0;
-	        console.log('getFile');
-	        fileReader.moi();
+		  var getFile = function (filesRead) {
+	        // console.log('getFile');
+	        // fileReader.moi();
 
-	        fileReader.readAsDataUrl(fileRead, scope)
-	          .then(function(result) {
-	              scope.imageSrc = result;
-	              console.log('scope.imageSrc:' + scope.imageSrc);
-	          });
-	        
-
-	        scope.imageSrcs = [];  
+	        imageSrcs = [];  
 	        for(var i=0; i < filesRead.length; i++) {
 		        fileReader.readAsDataUrl(filesRead[i], scope)
 		          .then(function(result) {
-		          	  var index = scope.imageSrcs.length;
-		              scope.imageSrcs[index] = result;
+		          	  var index = imageSrcs.length;
+		              imageSrcs[index] = result;
 		              // console.log('result: ' + result);
-		              console.log('scope.imageSrcs['+ index + ']: ' + scope.imageSrcs[index]);
+		              // console.log('imageSrcs['+ index + ']: ' + imageSrcs[index]);
 		          });
 	        }
 
+			scope[attrs['imageSrcs']] = imageSrcs;
 	      };
 	      
 	      element.bind('change', function(e) {
-	        // scope.file = (e.srcElement || e.target).files[0];
-	        var fileRead = (e.srcElement || e.target).files[0];
-	        console.log('file: '+ fileRead.name);	
 	        var filesRead = (e.srcElement || e.target).files;
-	        scope.getFile(fileRead, filesRead);
+	        getFile(filesRead);
 	      });
-
-
 
 	    }
 	  };
