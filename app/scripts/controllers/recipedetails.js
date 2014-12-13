21972e7da56d9f2ebc2a0c84d3adb875a3251f97
tkarling <tkarling@live.com>
2014-12-13 12:21:49 -0800

    local stroring & retrieving works from the fileSelect directive now

--- app/scripts/controllers/recipedetails.js
+++ app/scripts/controllers/recipedetails.js
@@ -9,6 +9,23 @@
  */
 
 angular.module('recipeshopperApp')
-  .controller('RecipeDetailsController', ['$scope', function ($scope) { 
+  .controller('RecipeDetailsController', ['$scope', function ($scope) { //, localStorageService
     currentTab=2;
+
+ 	// var imageSrcsInStore = [];
+	// if(localStorageService) {
+	// 	console.log('localStorageService exists');
+	// 	imageSrcsInStore = localStorageService.get('imageSrcs');
+	// 	console.log('imageSrcsInStore', imageSrcsInStore);
+	// }
+	// $scope.imageSrcs = imageSrcsInStore || [];
+	// console.log('$scope.imageSrcs', $scope.imageSrcs);
+
+ 	//  $scope.$watch('imageSrcs', function () {
+	// 	if(localStorageService) {
+	// 		localStorageService.set('imageSrcs', $scope.imageSrcs);
+	// 	  	console.log('$scope.imageSrcs after set ', $scope.imageSrcs);
+	// 	}
+	// }, true);
+
   }]);
--- app/scripts/services/filereader.js
+++ app/scripts/services/filereader.js
@@ -68,34 +68,53 @@
       };
    })
 
-  .directive('rsFileSelect', function(fileReader){
+  .directive('rsFileSelect', function(fileReader, localStorageService){
       return {
         link: function(scope, element, attrs) {
-          // console.log('ngFileSelect'); 
-          var imageSrcs = scope[attrs['imageSrcs']];
+            // console.log('ngFileSelect'); 
+            var imageSrcs = scope[attrs['imageSrcs']];
 
-        var getFile = function (filesRead) {
-            // console.log('getFile');
-            // fileReader.moi();
-
-            imageSrcs = [];  
-            for(var i=0; i < filesRead.length; i++) {
-              fileReader.readAsDataUrl(filesRead[i], scope)
-                .then(function(result) {
-                    var index = imageSrcs.length;
-                    imageSrcs[index] = result;
-                    // console.log('result: ' + result);
-                    // console.log('imageSrcs['+ index + ']: ' + imageSrcs[index]);
-                });
+            var imageSrcsInStore = [];
+            if(localStorageService) {
+              // console.log('localStorageService exists');
+              imageSrcsInStore = localStorageService.get('imageSrcs');
+              // console.log('imageSrcsInStore', imageSrcsInStore);
             }
+            imageSrcs = imageSrcsInStore || [];
 
-        scope[attrs['imageSrcs']] = imageSrcs;
-          };
-          
-          element.bind('change', function(e) {
-            var filesRead = (e.srcElement || e.target).files;
-            getFile(filesRead);
-          });
+            //save images to returning attr after reading from local storage
+            // console.log('scope.imageSrcs', imageSrcs);
+            scope[attrs['imageSrcs']] = imageSrcs;
+            // console.log('Refresh: After: scope[attrs ' + scope[attrs['imageSrcs']].length);
+
+            var getFile = function (filesRead) {
+                // console.log('getFile');
+                // fileReader.moi();
+
+                imageSrcs = [];  
+                for(var i=0; i < filesRead.length; i++) {
+                  fileReader.readAsDataUrl(filesRead[i], scope)
+                    .then(function(result) {
+                        var index = imageSrcs.length;
+                        imageSrcs[index] = result;
+                        // console.log('result: ' + result);
+                        // console.log('imageSrcs['+ index + ']: ' + imageSrcs[index]);
+                        if(index === (filesRead.length-1)) {
+                            // if this is the last file name, save images to local storage & returning attr
+                            scope[attrs['imageSrcs']] = imageSrcs;
+                            if(localStorageService) {
+                              localStorageService.set('imageSrcs', imageSrcs);
+                            }
+                        }
+                    });
+                }
+
+              };
+              
+            element.bind('change', function(e) {
+              var filesRead = (e.srcElement || e.target).files;
+              getFile(filesRead);
+            });
 
         }
       };
