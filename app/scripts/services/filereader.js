95fedd0b6f94452a1a8215e9ae63357ca2a1a889
tkarling <tkarling@live.com>
2014-12-16 15:08:28 -0800

    took updated version of filereader in use

--- app/scripts/services/filereader.js
+++ app/scripts/services/filereader.js
@@ -9,32 +9,35 @@
  */
   angular
   .module('fileReaderMod', [
+     'LocalStorageModule', 
   ]);
 
  angular.module('fileReaderMod')
-   .factory('fileReader', function ($q) {
+   .factory('fileReader', function ($q, $rootScope) {
+      // Reads selected pic from disk
+
       // console.log('init fileReader');
       return {
  
-        onLoad: function(reader, deferred, scope) {
+        onLoad: function(reader, deferred) { 
             return function () {
-                scope.$apply(function () {
+                $rootScope.$apply(function () {
                     deferred.resolve(reader.result);
                 });
             };
         },
 
-        onError: function (reader, deferred, scope) {
+        onError: function (reader, deferred) {  
             return function () {
-                scope.$apply(function () {
+                $rootScope.$apply(function () {
                     deferred.reject(reader.result);
                 });
             };
         },
 
-        onProgress: function(reader, scope) {
+        onProgress: function(reader) { 
             return function (event) {
-                scope.$broadcast('fileProgress',
+                $rootScope.$broadcast('fileProgress',
                     {
                         total: event.total,
                         loaded: event.loaded
@@ -50,11 +53,11 @@
             return reader;
         },
    
-        readAsDataUrl: function (file, scope) {
+        readAsDataUrl: function (file) { 
             // console.log('readAsDataUrl');
             var deferred = $q.defer();
              
-            var reader = this.getReader(deferred, scope);         
+            var reader = this.getReader(deferred);         
             reader.readAsDataURL(file);
              
             return deferred.promise;
@@ -68,52 +71,67 @@
       };
    })
 
-  .directive('rsFileSelect', function(fileReader, localStorageService){
+   .factory('fileListReader', function (fileReader, localStorageService, $rootScope) {
+    // Gets pics from local storage; updates pics in local storage to be selected files on disk
+      var imageSrcs = [];
+
+      if(imageSrcs.length === 0) {
+         // read images from local storage
+         var imageSrcsInStore = [];
+          if(localStorageService) {
+            // console.log('localStorageService exists');
+            imageSrcsInStore = localStorageService.get('imageSrcs');
+            // console.log('imageSrcsInStore', imageSrcsInStore);
+          }
+          imageSrcs = imageSrcsInStore || [];
+          $rootScope.$broadcast('handleSlidesUpdated');
+      }
+
+      return {
+        getImageSrcs: function () {
+          return imageSrcs;
+        }, 
+
+        updateImageSrcs: function (filesRead) {
+          imageSrcs = [];  
+          for(var i=0; i < filesRead.length; i++) {
+            fileReader.readAsDataUrl(filesRead[i])  
+              .then(function(result) {
+                  var index = imageSrcs.length;
+                  imageSrcs[index] = result;
+                  // console.log('result: ' + result);
+                  // console.log('imageSrcs['+ index + ']: ' + imageSrcs[index]);
+                  if(index === (filesRead.length-1)) {
+                      // if this is the last file name, save images to local storage & broadcast change
+                      if(localStorageService) {
+                        localStorageService.set('imageSrcs', imageSrcs);
+                        //console.log('files stored index:' + index);
+                      }
+                      $rootScope.$broadcast('handleSlidesUpdated');
+                  }
+              });
+          }
+        } 
+
+      };
+   })
+
+
+  .directive('rsFileSelect', function(fileReader, fileListReader, localStorageService, $rootScope){
       return {
         link: function(scope, element, attrs) {
             // console.log('ngFileSelect'); 
             var imageSrcs = scope[attrs['imageSrcs']];
 
-            var imageSrcsInStore = [];
-            if(localStorageService) {
-              // console.log('localStorageService exists');
-              imageSrcsInStore = localStorageService.get('imageSrcs');
-              // console.log('imageSrcsInStore', imageSrcsInStore);
-            }
-            imageSrcs = imageSrcsInStore || [];
+            scope[attrs['imageSrcs']] = fileListReader.getImageSrcs();
 
-            //save images to returning attr after reading from local storage
-            // console.log('scope.imageSrcs', imageSrcs);
-            scope[attrs['imageSrcs']] = imageSrcs;
-            // console.log('Refresh: After: scope[attrs ' + scope[attrs['imageSrcs']].length);
+            scope.$on('handleSlidesUpdated', function() {
+              scope[attrs['imageSrcs']] = fileListReader.getImageSrcs();
+            });
 
-            var getFile = function (filesRead) {
-                // console.log('getFile');
-                // fileReader.moi();
-
-                imageSrcs = [];  
-                for(var i=0; i < filesRead.length; i++) {
-                  fileReader.readAsDataUrl(filesRead[i], scope)
-                    .then(function(result) {
-                        var index = imageSrcs.length;
-                        imageSrcs[index] = result;
-                        // console.log('result: ' + result);
-                        // console.log('imageSrcs['+ index + ']: ' + imageSrcs[index]);
-                        if(index === (filesRead.length-1)) {
-                            // if this is the last file name, save images to local storage & returning attr
-                            scope[attrs['imageSrcs']] = imageSrcs;
-                            if(localStorageService) {
-                              localStorageService.set('imageSrcs', imageSrcs);
-                            }
-                        }
-                    });
-                }
-
-              };
-              
             element.bind('change', function(e) {
               var filesRead = (e.srcElement || e.target).files;
-              getFile(filesRead);
+              fileListReader.updateImageSrcs(filesRead);
             });
 
         }
