a6ff6b87520497c7e62acfd63e7818d620fe6fe1
tkarling <tkarling@live.com>
2015-01-04 19:50:03 -0800

    Unit testing 3: authentication service seems to be unit testable now (at least without actual tests :)

--- app/scripts/services/authentication.js
+++ app/scripts/services/authentication.js
@@ -7,103 +7,97 @@
  * # Authentication
  * Factory in the recipeshopperApp.
  */
-//  angular
-// .module('loginMod', [
-//    'firebase', 
-// ]);
 
- angular
+angular
 .module('loginMod', [
+  'firebase',
 ]);
 
-// angular.module('loginMod')
-//   .factory('Authentication', ['$log', '$firebase', 'FIREBASE_URL', '$location', '$rootScope', '$timeout', 
-//     function ($log, $firebase, FIREBASE_URL, $location, $rootScope, $timeout) {
 angular.module('loginMod')
   .config(function($logProvider) {
     $logProvider.debugEnabled(true);
   })
-  .factory('Authentication', ['$log','$location', '$rootScope', '$timeout', 'myDependency', 'FIREBASE_URL', 
-    function ($log, $location, $rootScope, $timeout, myDependency, FIREBASE_URL) {
+  .factory('Authentication', ['$log','$location', '$rootScope', '$timeout', '$firebase', 'FIREBASE_URL', 
+    function ($log, $location, $rootScope, $timeout, $firebase, FIREBASE_URL) {
 
-    // var data = {};
-    // data.userLoggedIn = false;
-    // var ref = new Firebase(FIREBASE_URL);
-    // var authData = ref.getAuth();
-    // if (authData) {
-    //   console.log("User " + authData.uid + " is logged in with " + authData.provider);
-    //   // console.log("logging out now");
-    //   // ref.unauth();
-    // } else {
-    //   console.log("User is logged out");
-    // }
+    var data = {};
+    data.userLoggedIn = false;
+    var ref = new Firebase(FIREBASE_URL);
+    // // var authData = ref.getAuth();
+    // // if (authData) {
+    // //   console.log("User " + authData.uid + " is logged in with " + authData.provider);
+    // //   // console.log("logging out now");
+    // //   // ref.unauth();
+    // // } else {
+    // //   console.log("User is logged out");
+    // // }
 
-    // var setUserEmail = function (userEmail) {
-    //     data.userEmail = userEmail;
-    //     data.userLoggedIn = (data.userEmail != undefined);
-    //     $timeout(function () {
-    //       // timeout needed to have time to create the controller receivig this
-    //       $rootScope.$broadcast('handleUserLoggedInChanged');
-    //     }, 100);
-    //     $log.debug("User email set", data.userEmail, userEmail);
-    //   } // setErrorMessage
+    var setUserEmail = function (userEmail) {
+        data.userEmail = userEmail;
+        data.userLoggedIn = (data.userEmail != undefined);
+        $timeout(function () {
+          // timeout needed to have time to create the controller receivig this
+          $rootScope.$broadcast('handleUserLoggedInChanged');
+        }, 100);
+        $log.debug("User email set", data.userEmail, userEmail);
+      } // setErrorMessage
 
-    // var authDataCallback = function(authData) {
-    //   console.log('authDataCallback called', authData);
-    //   if (authData) {
-    //     $log.debug("User " + authData.uid + " is logged in with " + authData.provider);
-    //     // console.log("authData: ", authData);
-    //     // $scope.$apply(setUserEmail(authData.password.email));
-    //     if(authData.password) {
-    //       setUserEmail(authData.password.email);
-    //     } else {
-    //       setUserEmail(undefined);
-    //     }
-    //   } else {
-    //     $log.debug$log.debug("User is logged out");
-    //     setUserEmail(undefined);
-    //   }
-    // } //authDataCallback
+    var authDataCallback = function(authData) {
+      $log.debug('authDataCallback called', authData);
+      if (authData) {
+        $log.debug("User " + authData.uid + " is logged in with " + authData.provider);
+        // console.log("authData: ", authData);
+        // $scope.$apply(setUserEmail(authData.password.email));
+        if(authData.password) {
+          setUserEmail(authData.password.email);
+        } else {
+          setUserEmail(undefined);
+        }
+      } else {
+        $log.debug("User is logged out");
+        setUserEmail(undefined);
+      }
+    } //authDataCallback
 
-    // ref.onAuth(authDataCallback);
+    ref.onAuth(authDataCallback);
 
     // Public API here
     return {
-      // login: function (user, authHandler) {
-      //   $log.debug(user.email);
-      //   ref.authWithPassword({
-      //     email: user.email,
-      //     password: user.password
-      //   }, authHandler);
-      // }, //login
+      login: function (user, authHandler) {
+        $log.debug(user.email);
+        ref.authWithPassword({
+          email: user.email,
+          password: user.password
+        }, authHandler);
+      }, //login
 
-      // register: function (user, authHandler) {
-      //   console.log(user.email);
-      //   ref.createUser({
-      //     email: user.email,
-      //     password: user.password
-      //   }, authHandler);
-      // }, //register
+      register: function (user, authHandler) {
+        $log.debug(user.email);
+        ref.createUser({
+          email: user.email,
+          password: user.password
+        }, authHandler);
+      }, //register
 
-      // logout: function() {
-      //   ref.unauth();
-      // }, // logout
+      logout: function() {
+        ref.unauth();
+      }, // logout
 
+      userEmail: function () {
+        return data.userEmail;
+      }, // userEmail
+
+      userLoggedIn: function () {
+        return data.userLoggedIn;
+      }, // userLoggedIn
+
+      // This is practice for setting up unit testing
       useDependency: function () {
         $log.log('useDependency');
         $log.debug('useDependency');
         console.log('console.log: useDependency');
-        return myDependency.getSomething();
+        return $firebase.getSomething();
       } // userEmail
-
-
-      // userEmail: function () {
-      //   return data.userEmail;
-      // }, // userEmail
-
-      // userLoggedIn: function () {
-      //   return data.userLoggedIn;
-      // } // userLoggedIn
       
     }; 
 
--- test/spec/services/authentication.js
+++ test/spec/services/authentication.js
@@ -18,7 +18,7 @@ describe('Service: Authentication', function () {
 
 
       module(function ($provide) {
-          $provide.value('myDependency', mockDependency);
+          $provide.value('$firebase', mockDependency);
           $provide.value('FIREBASE_URL', mockUrl);
       });
 
@@ -42,71 +42,13 @@ describe('Service: Authentication', function () {
       expect($log.debug.logs.length).toEqual(1);
   }));
 
+ it('hould write in debug log when loin is called', inject(function (Authentication, $log) {
+      var user = {};
+      user.email = 'moi@hei.com';
+      Authentication.login(user, null);
+      expect($log.debug.logs.length).toEqual(1);
+      // expect($log.debug.logs[0]).toEqual('moi@hei.com');
+  }));
 
 });
 
-// describe('Service: Authentication', function () {
-
-//   // load the service's module
-//   // beforeEach(
-//   //   module('mock.firebase');
-//   //   module('firebase');
-//   //   module('loginMod'););
-
-//   // $firebase, FIREBASE_URL, $location, $rootScope, $timeout
-
-//   // instantiate service
-//   // var authentication, $firebase, FIREBASE_URL, $location, $rootScope, $timeout;
-//   // beforeEach(function () {
-//   //   module('mock.utils');
-//   //   module('mock.firebase');
-//   //   module('firebase');
-//   //   module('loginMod');
-//   //   inject(function (_authentication_, _$firebase_, _FIREBASE_URL_, _$location_, _$rootScope_, _$timeout_) {
-//   //     authentication = _authentication_;
-//   //     $firebase = _$firebase_;
-//   //     FIREBASE_URL = _FIREBASE_URL_;
-//   //     $location = _$location_;
-//   //     $rootScope = _$rootScope_;
-//   //     $timeout = _$timeout_;
-//   //   })
-//   // });
-
-//   var authentication,
-//       mockDependency;
-
-//   beforeEach(module('loginMod'));
-
-//   beforeEach(function () {
-
-//       mockDependency = {
-//           // logout: function () {
-//           //     // return 'mockReturnValue';
-//           // }
-//           // Firebase: function (url) {
-//           //     return 'mockReturnValue';
-//           // },
-//           moi: function () {
-//               return 'mockReturnValue';
-//           }
-//       };
-
-//       module(function ($provide) {
-//           $provide.value('myDependency', mockDependency);
-//           // $provide.value('FIREBASE_URL', mockDependency);
-//       });
-
-
-//   });
-
-//   it('should do something', inject(function (authentication) {
-//     // expect(!!authentication).toBe(true);
-//     expect(authentication.moi()).toBe('mockReturnValue');
-//   }));
-
-//   // it('should do something', function () {
-//   //   // expect(!!authentication).toBe(true);
-//   //   expect(authentication.moi()).toBe('moi');
-//   // });
-
-// });
