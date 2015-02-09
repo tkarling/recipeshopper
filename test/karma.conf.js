0764f726b1e06a0460eb34e49a20a375deee6006
tkarling <tkarling@live.com>
2015-02-08 19:12:49 -0800

    added angular-messages to karma.conf to make unit tests pass again. Deleted some commented code

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -37,6 +37,7 @@ module.exports = function(config) {
       'bower_components/angular-aria/angular-aria.js',
       'bower_components/hammerjs/hammer.js',
       'bower_components/angular-material/angular-material.js',
+      'bower_components/angular-messages/angular-messages.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
