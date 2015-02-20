dfbbf51173a6296fc36729157d885b4b7e4d8135
tkarling <tkarling@live.com>
2015-02-19 18:12:11 -0800

    protractor test for adding and deleting recipe

--- test/e2e/spec.js
+++ test/e2e/spec.js
@@ -1,5 +1,16 @@
 // spec.js
+
 describe('angularjs homepage', function() {
+	var flow = protractor.promise.controlFlow();
+
+	function waitOne() {
+		return protractor.promise.delayed(1000);
+	}
+
+	function sleep() {
+		return flow.execute(waitOne);
+	}
+
 	var loginEmail = element(by.model('inputUser.email'));
 	var loginPassword = element(by.model('inputUser.password'));
     var loginButton = element(by.id('loginbutton'));
@@ -12,7 +23,7 @@ describe('angularjs homepage', function() {
 
 	var addInput1, addInput2, addButton;
 	var thisList, listItemField1, listItemField2,deleteButtonId;
-  	function shouldAddAndDeleteItem (field1Content, field2Content) {
+  	function shouldAddAndDeleteItem (field1Content, field2Content, separator) {
 
 		function addItem(a, b) {
 		    addInput1.sendKeys(a);
@@ -29,13 +40,13 @@ describe('angularjs homepage', function() {
 
 			thisList.filter(function(elem, index) {
 			  return elem.element(by.binding(listItemField1)).getText().then(function(text) {
-			    return text === 'carrots';
+			    return text === field1Content;
 			  });
 			}).then(function(filteredElements) {
 				// check content of new item
 				newItem = filteredElements[0];
 				expect(newItem.element(by.binding(listItemField1)).getText()).toEqual(field1Content);
-				expect(newItem.element(by.binding(listItemField2)).getText()).toEqual(field2Content + ';');
+				expect(newItem.element(by.binding(listItemField2)).getText()).toEqual(field2Content + separator);
 
 				// delete item
 				newItem.element(by.id(deleteButtonId)).click();
@@ -45,8 +56,8 @@ describe('angularjs homepage', function() {
   	}
 
 	beforeEach(function() {
+		// sleep();
 		browser.get('http://localhost:9005/#/login');
-		login();
 	});
 
 	// it('should have a title', function() {
@@ -55,6 +66,10 @@ describe('angularjs homepage', function() {
 
 
 	it('should add and delete a product on shopping list', function() {
+		login();
+
+	    expect(browser.getCurrentUrl()).toBe('http://localhost:9005/#/main');
+
 		addInput1 = element(by.model('product'));
 		addInput2 = element(by.model('aisle'));
 	    addButton = element(by.id('addproductbutton'));
@@ -63,22 +78,32 @@ describe('angularjs homepage', function() {
 		listItemField2 = 'aisle';
 		deleteButtonId = 'deleteproductbutton'
 
-		shouldAddAndDeleteItem('carrots', 'veggies');
+		shouldAddAndDeleteItem('carrots', 'veggies', ';');
 	});
+
+	it('should add and delete a recipe on recipe list', function() {
+		// not needed at the momen as the test opens browser with menu open
+	    // var menuButton = element(by.id('menubutton'));
+	    // menuButton.click();
+	    // expect(browser.getCurrentUrl()).toBe('http://localhost:9005/#/main');
+
+	    var recipeListMenuButton = element(by.id('recipelistmenubutton'));
+	    recipeListMenuButton.click();
+	    expect(browser.getCurrentUrl()).toBe('http://localhost:9005/#/recipelist');
+
+	    addInput1 = element(by.model('recipename'));
+		addInput2 = element(by.model('category'));
+	    addButton = element(by.id('addrecipebutton'));
+		thisList = element.all(by.repeater('item in recipes'));
+		listItemField1 = 'recipename';
+		listItemField2 = 'category';
+		deleteButtonId = 'deleterecipebutton'
+
+		shouldAddAndDeleteItem('soup', 'thanksgiving', '');
+
+	});
+
 
 });
 
-		// var recipeName = element(by.model('recipename'));
-		// var category = element(by.model('category'));
-	 //    var addButton = element(by.id('addbutton'));
-		// var recipeList = element.all(by.repeater('item in recipes'));
-
-		// function add(a, b) {
-		//     recipeName.sendKeys(a);
-		//     category.sendKeys(b);
-		//     addButton.click();
-	 //  	}
-
-		// add('soup', 'thanksgiving');
-		// expect(recipeList.count()).toEqual(4);
 
